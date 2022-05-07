5491cd6b2e77244aec510012f9cea8da755deff5
Jonathan <jonathanffarias@hotmail.com>
2022-05-06 22:04:28 -0300

    Aula 06/05

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -7,31 +7,30 @@ use Spatie\Permission\Models\Role;
 use Spatie\Permission\Models\Permission;
 use Illuminate\Support\Facades\DB;
 
-
 class RoleController extends Controller
 {
-    public function __construct(){
-
-        $this->middleware('permission:role-list|role-create|role-edit|role-delete',
+    public function __construct()
+    {
+        $this->middleware(  'permission:role-list|role-create|role-edit|role-delete',
                             ['only' => ['index','store']]);
-        $this->middleware('permission:role-create', ['only' => ['create','store']]);
-        $this->middleware('permission:role-edit', ['only' => ['edit','update']]);
-        $this->middleware('permission:role-delete', ['only' => ['destroy']]);
-    }    
+        $this->middleware(  'permission:role-create',
+                            ['only' => ['create','store']]);
+        $this->middleware(  'permission:role-edit',
+                            ['only' => ['edit','update']]);
+        $this->middleware(  'permission:role-delete',
+                            ['only' => ['destroy']]);
+    }
 
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index( Request $request)
+    public function index()
     {
-        $qtd_por_pagina = 5;
+        $roles = Role::orderBy('id','DESC')->paginate(5);
 
-        $roles = Role::orderBy('id', 'DESC')->paginate($qtd_por_pagina);
-
-        return view('roles.index', compact('roles'))->
-                with('i', ($request->input('page', 1) - 1) * $qtd_por_pagina);
+        return view('roles.index', compact('roles'))->with('i', ($request->input('page', 1) - 1) * 5);
     }
 
     /**
@@ -56,13 +55,12 @@ class RoleController extends Controller
     {
         $this->validate($request, [ 'name' => 'required|unique:roles,name',
                                     'permission' => 'required']);
-
+        
         $role = Role::create(['name' => $request->input('name')]);
 
         $role->syncPermissions($request->input('permission'));
 
-        return redirect()->route('roles.index')->
-                with('success', 'Perfil criado com sucesso');
+        return redirect()->route('roles.index')->with('success', 'Perfil criado com sucesso');
     }
 
     /**
@@ -75,14 +73,12 @@ class RoleController extends Controller
     {
         $role = Role::find($id);
 
-        $rolePermissions = Permission::join(
-                                        'role_has_permissions',
-                                        'role_has_permissions.permission_id',
-                                        '=',
-                                        'permissions.id')->
-                                        where('role_has_permissions.role_id', $id)->get();
-
-        return view('roles.show', compact('role','rolePermissions'));
+        $rolePermissions = Permission::join('role_has_permissions',
+                                            'role_has_permissions.permission_id',
+                                            '=',
+                                            'permission_id')->where('role_has_permission.role_id', $id)->get();
+        
+        return view('roles.show', compact('role', 'rolePermissions'));                                    
     }
 
     /**
@@ -97,12 +93,13 @@ class RoleController extends Controller
 
         $permission = Permission::get();
 
-        $rolePermissions = DB::table('role_has_permissions')->
-                                where('role_has_permissions.role_id', $id)->
-                                pluck('role_has_permissions.permission_id')->all();
+        $rolePermissions = DB::table('role_has_permissions')
+                                ->where('role_has_permissions.role_id', $id)
+                                ->pluck('role_has_permissions.permission_id')
+                                ->all();
 
         return view('roles.edit',
-                    compact('role','permission','rolePermissions'));
+                        compact('role','permission','rolePermissions'));
     }
 
     /**
@@ -120,9 +117,10 @@ class RoleController extends Controller
         $role = Role::find($id);
         $role->name = $request->input('name');
         $role->save();
-        $role->syncPermissions($request->input('permission'));
+        $role->syncPerminssions($request->input('permission'));
 
-        return redirect()->route('roles.index')->with('success', 'Perfil atualizado com sucesso');
+        return redirect()->route('roles.index')
+                            ->with('success', 'Perfil atualizado com sucesso');
     }
 
     /**
@@ -133,8 +131,8 @@ class RoleController extends Controller
      */
     public function destroy($id)
     {
-        DB::table('roles')->where('id',$id)->delete();
+        DB::table('roles')->where('id', $id)->delete();
 
-        return redirect()->route('roles.index')->with('success', 'Perfil apagado');
+        return redirect()->route('roles.index')->with('success', 'Perfil apagado com sucesso');
     }
 }
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,36 +4,25 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Arr;
-use App\Models\User;
 use Spatie\Permission\Models\Role;
 use DB;
 use Hash;
+use App\Models\User;
 
 class UserController extends Controller
 {
-    //Essa é uma forma de controlar o acesso
-    public function __construct()
-    {
-        $this->middleware('permission:user-list',['only' => ['index','show']]);
-        $this->middleware('permission:user-create',['only' => ['create','store']]);
-        $this->middleware('permission:user-edit',['only' => ['edit','update']]);
-        $this->middleware('permission:user-delete',['only' => ['destroy']]);
-    }
-
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index( Request $request)
+    public function index(Request $request)
     {
-        $qtd_por_pagina = 5;
-
-        $data = User::orderBy('id', 'DESC')->paginate($qtd_por_pagina);
+        $data = User::orderBy('id','DESC')->paginate(5);
 
         return view('users.index', 
-                compact('data'))->
-                    with('i', ($request->input('page', 1) - 1) * $qtd_por_pagina);
+                    compact('data'))->with('i',
+                                        ($request->input('page', 1) - 1) * 5);
     }
 
     /**
@@ -56,21 +45,19 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        $this->validate($request,
-                        ['name' => 'required',
-                        'email' => 'required|email|unique:users,email',
-                        'password' => 'required|same:confirm-password',
-                        'roles' => 'required']);
-
+        $this->validate($request, [ 'name' => 'required',
+                                    'email' => 'required|email|unique:users,email',
+                                    'password' => 'required|same:confirm-password',
+                                    'roles' => 'required']);
         $input = $request->all();
 
         $input['password'] = Hash::make($input['password']);
 
         $user = User::create($input);
 
-        $user->assignRole( $request->input('roles'));
+        $user->assignRole($request->input('roles'));
 
-        return redirect()->route('users.index')->with('success','Usuário criado com sucesso');
+        return redirect()->route('users.index')->with('success', 'Usuário criado com sucesso');
     }
 
     /**
@@ -112,29 +99,24 @@ class UserController extends Controller
      */
     public function update(Request $request, $id)
     {
-        $this->validate($request,
-                        ['name' => 'required',
-                        'email' => 'required|email',
-                        'password' => 'required|same:confirm-password',
-                        'roles' => 'required']);
-
+        $this->validate($request, [ 'name' => 'required',
+                                    'email' => 'required|email|unique:users,email',
+                                    'password' => 'required|same:confirm-password',
+                                    'roles' => 'required']);
+        
         $input = $request->all();
 
         if(!empty($input['password'])){
-
-            $input['password'] = Hash::make($input['password']);
-
+            $input['password'] = Hash::make($input['password']);            
         }else{
-
-            $input = Arr::except( $input, ['password']);
+            $input = Arr::except($input, array('password'));
         }
 
         $user = User::find($id);
 
         $user->update($input);
 
-        DB::table('model_has_roles')->where('model_id',$id)->delete();
-
+        DB::table('model_has_roles')->where('model_id', $id)->delete();
         $user->assignRole($request->input('roles'));
 
         return redirect()->route('users.index')->with('success', 'Usuário atualizado com sucesso');
@@ -150,6 +132,6 @@ class UserController extends Controller
     {
         User::find($id)->delete();
 
-        return redirect()->route('users.index')->with('success','Usuário removido com sucesso');
+        return redirect()->route('users.index')->with('success', 'Usuário removido com sucesso');
     }
 }
